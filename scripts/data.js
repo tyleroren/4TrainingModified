68114db7cf02ee3f6c97d96131d43b96276a85d5
Tyler Oren <131497714+tyleroren@users.noreply.github.com>
2025-03-06 15:50:46 -0500

    Add modified version of files with sensitive info removed

---
+++ scripts/data.js
@@ -0,0 +1,145 @@
+/*
+This file contains an array of objects. Each object contains the properties of one video.
+The web pages are built off of the properties of these objects
+*/
+
+const materials = [
+    {
+        category: "helpdesk",
+        title: "Ticket Flow / Escalation",
+        filename: "ticketflow.pdf",
+        author: "Tyler Oren",
+        date: "07/11/2024",
+        index: []
+    },
+    {
+        category: "helpdesk",
+        title: "Connectwise Overview",
+        filename: "connectwise.mp4",
+        author: "Tyler Oren",
+        date: "06/13/2023",
+        index: [
+            "00:00", "Connectwise Login and Introduction",
+            "02:49", "Connectwise Navigation",
+            "04:09", "Introduction to the Service Board",
+            "07:22", "Introduction to Tickets",
+            "12:56", "Creating a New Ticket",
+            "20:09", "Adding Notes to a Ticket / Intro to Time Entry",
+            "35:19", "Time Sheet Management",
+            "44:55", "Customizing Connectwise"
+        ]
+    },
+    {
+        category: "helpdesk",
+        title: "Answering Calls (coming soon)",
+        filename: "",
+        author: "",
+        date: "",
+        index: []
+    },
+    {
+        category: "helpdesk",
+        title: "Troubleshooting on a Call (coming soon)",
+        filename: "",
+        author: "",
+        date: "",
+        index: []
+    },
+    {
+        category: "helpdesk",
+        title: "Troubleshooting Basics (coming soon)",
+        filename: "",
+        author: "",
+        date: "",
+        index: []
+    },
+    {
+        category: "helpdesk",
+        title: "Advanced Troubleshooting (coming soon)",
+        filename: "",
+        author: "",
+        date: "",
+        index: []
+    },
+    {
+        category: "helpdesk",
+        title: "Microsoft Licensing (coming soon)",
+        filename: "",
+        author: "",
+        date: "",
+        index: []
+    },
+    {
+        category: "sales",
+        title: "Microsoft Tenant Information",
+        filename: "microsoft.mp4",
+        author: "Charlie",
+        date: "10/06/2021",
+        index: [
+            "01:00", "New tenet setup / advisor accounts",
+            "05:10", "New tenet creation from beginning",
+            "10:00", "Migration notes",
+            "17:00", "Setup new tenent w/o ms account",
+            "20:00", "Domain provision",
+            "31:30", "Datto setup",
+            "39:00", "Datto Recovery",
+            "44:00", "Office Price Increase"
+        ]
+    },
+    {
+        category: "sales",
+        title: "Microsoft Licensing (coming soon)",
+        filename: "",
+        author: "",
+        date: "",
+        index: []
+    },
+    {
+        category: "lunch",
+        title: "Mike Lunch & Lead Part 1",
+        filename: "LunchandLeadVideo1.mp4",
+        author: "Mike",
+        date: "03/24/2023",
+        index: []
+    },
+    {
+        category: "lunch",
+        title: "Mike Lunch & Lead Part 2",
+        filename: "LunchandLeadVideo2.mp4",
+        author: "Mike",
+        date: "03/24/2023",
+        index: []
+    },
+    {
+        category: "lunch",
+        title: "Backups and EDR",
+        filename: "BackUpandEDR.mp4",
+        author: "Jakob",
+        date: "03/08/2023",
+        index: []
+    },
+    {
+        category: "lunch",
+        title: "DNS in Active Directory",
+        filename: "TonyADDNS.MP4",
+        author: "Tony",
+        date: "09/16/2022",
+        index: []
+    },
+    {
+        category: "lunch",
+        title: "Connectwise",
+        filename: "DerekandConnectwise.MP4",
+        author: "Derek",
+        date: "08/19/2022",
+        index: []
+    },
+    {
+        category: "lunch",
+        title: "Office 365",
+        filename: "JonandOffice365.MP4",
+        author: "Jon",
+        date: "08/05/2022",
+        index: []
+    },
+];
---
+++ scripts/script.js
@@ -0,0 +1,103 @@
+const videoFormats = ['mp4', 'MP4'];
+const docFormats = ['pdf', 'PDF'];
+
+// Creates listeners for every button on the page, passing the class and text to the createVideoList function
+document.querySelectorAll('button').forEach((item) => item.addEventListener('click', (e) => {
+    createVideoList(e.target.classList[0], e.target.textContent);
+}));
+
+// Creates the list of videos that appears depending on which button is pressed
+function createVideoList(source, header) {
+    // home button just refreshes the page because I'm lazy
+    if (source === "home") location.reload();
+    // Creates variables for html elements for easy access
+    const timeStamp = document.querySelector('.timeStamp');
+    const vidList = document.querySelector('.vidList');
+    const player = document.getElementById('videoPlayer');
+    // Removes any items that may be in the video list or index lists on the page
+    while (vidList.firstChild) vidList.removeChild(vidList.lastChild);
+    while (timeStamp.firstChild) timeStamp.removeChild(timeStamp.lastChild);
+    // Add header text, hide video and clear video path
+    document.querySelector('.videoTitle').textContent = "";
+    document.querySelector('.videoHeader').textContent = header;
+    document.getElementById('documentPlayer').classList.add('invisible');
+    player.pause()
+    player.removeAttribute('src');
+    player.load()
+    player.classList.add('invisible');
+    document.querySelector('.credits').textContent = "";
+    /* Runs through each object in the materials array, checking to see if the "category" property
+    matches the class of the button that was pressed. Each item that does match will create a hyperlink
+    and a list item. The hyperlink text is taken from the "title" property and the URL is a new function
+    which passes the "filename" property, "title" property, and the index of the array to the next function */
+    materials.forEach((item, index) => {
+        if (item.category === source) {
+            let list = document.createElement('li');
+            let a = document.createElement('a');
+            a.textContent = item.title;
+            a.href = "javascript:void(0);";
+            a.setAttribute("onclick", `timeStampSelection('${item.filename}', '${item.title}', ${index}, '${item.author}', '${item.date}')`);
+            list.appendChild(a);
+            vidList.appendChild(list);
+        };
+    });
+}
+
+// Creates the list of index timestamps depending on which video is selected
+function timeStampSelection(filename, title, selector, author, date) {
+    // Creates variables for html elements for easy access
+    const timeStamp = document.querySelector('.timeStamp');
+    const videoTitle = document.querySelector('.videoTitle');
+    const player = document.getElementById('videoPlayer');
+    const docPlayer = document.getElementById('documentPlayer');
+    let href = "";
+    // Removes any items that may be in the index list on the page
+    while (timeStamp.firstChild) timeStamp.removeChild(timeStamp.lastChild);
+    player.pause()
+    player.removeAttribute('src');
+    player.load()
+    player.classList.add('invisible');
+    docPlayer.classList.add('invisible');
+    videoTitle.textContent = title;
+    document.querySelector('.credits').textContent = `Created by ${author} on ${date}`;
+    // Choose file type
+    if (docFormats.includes(filename.slice(-3))) {
+        docPlayer.setAttribute('data', `videos/${filename}#navpanes=0`);
+        docPlayer.classList.remove('invisible');
+    } else if (videoFormats.includes(filename.slice(-3))) {
+        // Add video title and show the video player and sets the file path for the video
+        player.setAttribute('src', `videos/${filename}`);
+        player.classList.remove('invisible');
+        /* Using the array index that was passed from the previous function, we can target the index
+        property for the correct video in the array. Again a hyperlink and list item are created
+        for each item. Each even array item is saved to the href variable (this should be the timestamp)
+        and every odd array item is set as the hyperlink text (this should be the index label).
+        Again each hyperlink points to a new function, videoPlayer, passing through the filename and
+        timestamp */
+        materials[selector].index.forEach((item, index) => {
+            let list = document.createElement('li');
+            let a = document.createElement('a');
+            if (index % 2 === 0) {
+                href = item;
+            } else {
+                a.textContent = `${item} [${href}]`;
+                a.href = "javascript:void(0);";
+                a.setAttribute("onclick", `videoPlayer('${href}', '${filename}');`);
+                list.appendChild(a);
+                timeStamp.appendChild(list);
+            };
+        });
+    } else if (!filename.slice(-3)) {
+        videoTitle.textContent = "No video uploaded yet";
+    } else {
+        videoTitle.textContent = "! File type error !";
+    }
+    
+
+
+}
+
+// Sets the video to start at a specific time
+function videoPlayer(time, filename) {
+    document.querySelector('#videoPlayer').setAttribute('src', `videos/${filename}#t=${time}`);
+}
